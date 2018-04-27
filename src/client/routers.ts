d5a239a7666a84dee516bedf6ae6f930b72ebcdc
koubo <koubo@meituan.com>
2018-04-27 15:27:31 +0800

    解决ts不能显示css的问题 但依然不能在服务端找到正确的组件

--- src/client/routers.ts
+++ src/client/routers.ts
@@ -1,34 +1,32 @@
-/* tslint:disable */
+/* tslint:disable variable-name ordered-imports max-line-length */
 // client/routes.js
 import * as React from 'react';
 import universal from 'react-universal-component';
-import importCss from 'babel-plugin-universal-import/importCss.js'
-
+// import importCss from 'babel-plugin-universal-import/importCss.js';
+const importCss = require('babel-plugin-universal-import/importCss.js');
+// import universalImport from 'babel-plugin-universal-import/universalImport.js';
+const universalImport = require('babel-plugin-universal-import/universalImport.js');
 import AppRoot from './container/root';
 
-// 由于 ts-node/register 无法使用 babel-plugin-universal-import 所以我们需要将 babel-plugin-universal-import 转换后的代码明写出来
-// 实际上这个 plugin 只是在 babylon 处理 import 语法之前处理 import 语法
+/**
+ * 由于 ts-node/register 无法使用 babel-plugin-universal-import
+ * 所以我们需要将 babel-plugin-universal-import 转换后的代码明写出来
+ * 实际上这个 plugin 只是在 babylon 处理 import 语法之前处理 import 语法
+ */
 
-// TODO
-// const load = props => {
-//   console.log(props);
-//   return Promise.all([
-//     import('./container/home'),
-//     importCss('./container/home/home.css')
-//   ]).then(proms => proms[0]);
-// }
-
-const UniversalHome = universal(() => import('./container/home'), {
-  resolve: () => require.resolveWeak('./container/home'),
-  chunkName: 'container/home',
-  minDelay: 500,
-});
-
-// const UniversalHome = universal(load, {
+// const UniversalHome = universal(() => import('./container/home'), {
 //   resolve: () => require.resolveWeak('./container/home'),
 //   chunkName: 'container/home',
 //   minDelay: 500,
 // });
+
+const UniversalHome = universal(universalImport({
+  chunkName: () => 'container/home',
+  load: () => Promise.all([
+    import('./container/home'),
+    importCss('container/home')
+  ]).then(proms => proms[0])
+}));
 
 const UniversalProfile = universal(() => import('./container/profile'), {
   resolve: () => require.resolveWeak('./container/profile'),
