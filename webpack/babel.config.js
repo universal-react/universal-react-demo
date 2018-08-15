aafc426734a1f9e2447288a74da15ec9e281f6d0
Mrkou47 <koubodehao@gmail.com>
2018-08-15 20:54:46 +0800

    update babel & remove some unused pkg

--- webpack/babel.config.js
+++ webpack/babel.config.js
@@ -2,16 +2,20 @@ const babelConfig = {
   presets: [
     ["@babel/env", {
       targets: {
-        ie: "10",
-        firefox: "60",
-        chrome: "67",
-        safari: "11.1"
+        browsers: [
+          "android > 4",
+          "last 2 versions",
+          "ios 9"
+        ]
       },
-      useBuiltIns: "usage"
+      useBuiltIns: "usage",
+      debug: true,
     }],
     "@babel/react"
   ],
-  plugins: ["universal-import"]
+  plugins: ["universal-import", "@babel/plugin-syntax-dynamic-import"],
+  babelrc: false,
+
 };
 
 module.exports = babelConfig;
